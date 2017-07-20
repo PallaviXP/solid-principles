82c2b999ac1269897f94c68158716875277ca96a
Pradip Hudekar <phudekar@equalexperts.com>
2017-07-20 10:06:10 +0530

    added srp example

---
+++ solid/SRP1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace solid
+{
+  public class CustomerService
+  {
+    private readonly IRepository<Customer> customers;
+
+    public CustomerService(IRepository<Customer> customers)
+    {
+      this.customers = customers;
+    }
+
+    public Customer UpdateCustomer(Customer customer)
+    {
+      if (String.IsNullOrEmpty(customer.Email)
+      || String.IsNullOrEmpty(customer.Name))
+      {
+        throw new InvalidCustomerException();
+      }
+
+      var existingCustomer = customers.GetById(customer.ID);
+      if (existingCustomer == null)
+      {
+        throw new CustomerNotFoundException();
+      }
+
+      var updatedCustomer = customers.Update(customer);
+
+      return updatedCustomer;
+    }
+
+  }
+
+
+
+  public class Customer
+  {
+    public long ID { get; }
+    public string Name { get; }
+    public string Email { get; }
+  }
+
+  public interface IRepository<T>
+  {
+    T Update(T obj);
+
+    T GetById(long id);
+  }
+
+  public class InvalidCustomerException : Exception
+  {
+  }
+
+  public class CustomerNotFoundException : Exception
+  {
+  }
+}
